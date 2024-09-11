da67919cfcbbf4ce56de6202e30144c6103ef36f
Eduardo Henrique <126426078+EduardoHenriqueMS@users.noreply.github.com>
2024-09-11 16:31:45 -0300

    Delete Projeto Final directory

--- Projeto Final/R/grafico.R
+++
@@ -1,5 +0,0 @@
-grafico_status <- function(dados, coordenadax, coordenaday){
-	ggplot2::ggplot(dados, ggplot2::aes(x = {{coordenadax}}, y = {{coordenaday}})) +
-	ggplot2::geom_col(fill = "darkblue") +
-	ggplot2::geom_label(ggplot2::aes(x = {{coordenadax}}, y = {{coordenaday}}, label = {{coordenaday}}), color = "white", fill = "darkblue")
-}
